94b8b6cf35fa1b114129057a34d098fe49186c2a
Paul Nakata <paul@pmn.org>
2020-07-12 15:03:19 -0700

    oops, broke the default theme

--- src/themes.nim
+++ src/themes.nim
@@ -4,7 +4,7 @@ type
     Theme* = object
         background*, brand*, brandtext*, text*: string
 
-const default = Theme(background: "#E7F4FF", brand: "#FFFFFF", brandtext: "#FFFFFF", text: "#2F3F4E")
+const default = Theme(background: "#E7F4FF", brand: "#445F77", brandtext: "#FFFFFF", text: "#2F3F4E")
 const red = Theme(background: "#e6003a", brand: "#9a0027", brandtext: "#FFFFFF", text: "#FFFFFF")
 const blue = Theme(background: "#0080ff", brand: "#005ab3", brandtext: "#FFFFFF", text: "#FFFFFF")
 const green = Theme(background: "#ACDF87", brand: "#1E5631", brandtext: "#FFFFFF", text: "#1E5631")
